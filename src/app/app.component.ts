31a49a3e11ff7268fef99d3bdaab47fa8ee87480
Paul Santiago <paulsantiago282@gmail.com>
2023-07-30 14:02:43 -0400

    Output from game controller to app, input values in odd and even from app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'property-event-binding-encapsulation-assignment';
+ // TODO we need to pass this as inputs to the odd and even components
+ evenNumber = 0;
+ oddNumber = 0;
+
+  oddNumberGenerated(oddData: {odd:number}){
+     this.oddNumber = oddData.odd;
+  }
+
+  evenNumberGenerated(evenData: {even: number}){
+    this.evenNumber = evenData.even;
+  }
 }
+
--- src/app/gamecontrol/gamecontrol.component.ts
+++ src/app/gamecontrol/gamecontrol.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-gamecontrol',
   templateUrl: './gamecontrol.component.html',
   styleUrls: ['./gamecontrol.component.css']
 })
-export class GamecontrolComponent {
+export class GamecontrolComponent implements OnInit {
 
+  // TODO need to output a property for odd and even properties to app
+  @Output() oddNumberCreated = new EventEmitter<{odd: number}>();
+  @Output() evenNumberCreated = new EventEmitter<{even: number}> ();
+  //1. Odd
+  odd = 1;
+
+  //2. Even
+  even = 2;
+
+// TODO add a reference to set the interval of the game and to clear the interval
+
+
+
+
+  constructor() {}
+
+  ngOnInit(): void {
+
+  }
+  onStartGame(){
+    // TODO generate evens and odds on a set interval
+    this.generateOddNumbers();
+    this.generateEvenNumbers();
+  }
+   onEndGame(){
+    // stop the interal
+  }
+
+  generateOddNumbers(){
+    this.oddNumberCreated.emit({odd: this.odd})
+  }
+
+  generateEvenNumbers(){
+    this.evenNumberCreated.emit({even: this.even})
+  }
 }
